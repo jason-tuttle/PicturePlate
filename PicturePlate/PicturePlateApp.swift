9ae148e887979da0c6cf3086c05ac7a10e1085b8
Jason Tuttle <jason.tuttle@greenlight.guru>
2023-08-27 13:27:40 -0400

    Initial Commit

---
+++ PicturePlate/PicturePlateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicturePlateApp.swift
+//  PicturePlate
+//
+//  Created by Jason Tuttle on 8/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PicturePlateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
