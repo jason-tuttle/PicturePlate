0d70589c8748659871bd505f9edee23dc423d382
Jason Tuttle <jason.tuttle@greenlight.guru>
2023-08-28 20:03:14 -0400

    initial commit, attempting to add dragging functionality

--- PicturePlate/ContentView.swift
+++ PicturePlate/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    let foodImages = ["Hamburger", "Spaghetti", "FriedChicken", "Taco"]
+    @State private var droppedImage = Image(systemName: "photo")
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            droppedImage
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300)
+                .foregroundColor(.blue)
+                .dropDestination(for: Image.self) { items, location in
+                    droppedImage = items.first ?? Image(systemName: "questionmark.diamond")
+                    return true
+                }
+                
+            HStack {
+                Image(systemName: "square")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 125)
+                    .foregroundColor(.green)
+                    .padding()
+                Image(systemName: "square")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 125)
+                    .foregroundColor(.red)
+                    .padding()
+            }
+            
+                HStack {
+                    ForEach(foodImages, id: \.self) {
+                        Image($0)
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .draggable(Image($0))
+                    }
+                }
+            }
+            .padding()
     }
 }
 
