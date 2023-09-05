da74a9fb572bb40999d859e0f05b25fc907a1b0b
Jason Tuttle <jason.tuttle@greenlight.guru>
2023-09-05 19:15:21 -0400

    add veggies and image borders

--- PicturePlate/ContentView.swift
+++ PicturePlate/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     let foodImages = ["Hamburger", "Spaghetti", "FriedChicken", "Taco"]
-    @State private var droppedImage = Image(systemName: "photo")
+    let veggies = ["Broccoli", "Carrots", "Corn", "Green Bell Pepper", "Red Bell Pepper", "Peas"]
+    @State private var droppedImage = Image(systemName: "fish")
+    @State private var droppedVeggie = Image(systemName: "carrot")
+    @State private var droppedFruit = Image(systemName: "leaf")
     
     var body: some View {
         VStack {
@@ -18,36 +21,68 @@ struct ContentView: View {
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 300)
                 .foregroundColor(.blue)
+                .padding()
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(.blue, lineWidth: 10))
                 .dropDestination(for: Image.self) { items, location in
                     droppedImage = items.first ?? Image(systemName: "questionmark.diamond")
                     return true
                 }
-                
+            
             HStack {
-                Image(systemName: "square")
+                droppedVeggie
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 125)
+                    .frame(width: 125, height: 125)
                     .foregroundColor(.green)
                     .padding()
-                Image(systemName: "square")
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(.green, lineWidth: 8))
+                    .dropDestination(for: Image.self) { items, location in
+                        droppedVeggie = items.first ?? Image(systemName: "questionmark.diamond")
+                        return true
+                    }
+                    .padding(.horizontal)
+                droppedFruit
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 125)
+                    .frame(width: 125, height: 125)
                     .foregroundColor(.red)
                     .padding()
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(.red, lineWidth: 8))
+                    .dropDestination(for: Image.self) { items, location in
+                        droppedFruit = items.first ?? Image(systemName: "questionmark.diamond")
+                        return true
+                    }
             }
-            
-                HStack {
+            .padding()
+            ScrollView(.horizontal) {
+                HStack(alignment: .center) {
                     ForEach(foodImages, id: \.self) {
                         Image($0)
                             .resizable()
                             .frame(width: 100, height: 100)
+                            .overlay(RoundedRectangle(cornerRadius: 10).stroke(.blue, lineWidth: 5))
                             .draggable(Image($0))
                     }
                 }
+                .padding(10)
             }
-            .padding()
+            ScrollView(.horizontal) {
+                HStack(alignment: .center) {
+                    ForEach(veggies, id: \.self) {
+                        Image("Veg_\($0)")
+                            .resizable()
+                            .frame(width: 80, height: 80)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.green, lineWidth: 5)
+                            )
+                            .draggable(Image("Veg_\($0)"))
+                            .accessibilityLabel("Vegatable, \($0)")
+                    }
+                }
+                .padding(10)
+            }
+        }
     }
 }
 
